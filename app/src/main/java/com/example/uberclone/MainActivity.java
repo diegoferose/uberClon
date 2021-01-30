dd3cd3a426cbfb8e132b81f26eccf0fdf849ee5b
diegoferose <diegoferose8@gmail.com>
2021-01-30 15:19:25 -0500

    se agrega fucion a los botones de inicio en MainActivity

--- app/src/main/java/com/example/uberclone/MainActivity.java
+++ app/src/main/java/com/example/uberclone/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button mButtonDriver;
+    Button mButtonClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonDriver = findViewById(R.id.btnIamDriver);
+        mButtonClient = findViewById(R.id.btnIamClient);
+
+        mButtonClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+        mButtonDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToSelectAuth();
+            }
+        });
+    }
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this,SelectOptionAuthActivity.class);
+        startActivity(intent);
+
     }
 }
