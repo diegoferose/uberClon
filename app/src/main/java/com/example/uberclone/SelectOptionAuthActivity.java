5df4f964d8a97a2e9d1b1b553ed75935ce5cee5b
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-02-03 22:58:03 -0500

    Creacion de action_bar_toollbar.xml

--- app/src/main/java/com/example/uberclone/SelectOptionAuthActivity.java
+++ app/src/main/java/com/example/uberclone/SelectOptionAuthActivity.java
@@ -1,14 +1,22 @@
 package com.example.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+       getSupportActionBar().setTitle("Selecionar Opcion");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
