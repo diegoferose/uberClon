1689cfe92f5ef57fc60dc47ac032de0dfda45a23
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-02-08 23:08:04 -0500

    Se realiza login , funcional con validacion en  firebase

--- app/src/main/java/com/example/uberclone/SelectOptionAuthActivity.java
+++ app/src/main/java/com/example/uberclone/SelectOptionAuthActivity.java
@@ -3,11 +3,15 @@ package com.example.uberclone;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
     Toolbar mToolbar;
+    Button mButtonGoToLogin;
 
 
     @Override
@@ -18,5 +22,19 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
        getSupportActionBar().setTitle("Selecionar Opcion");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin = findViewById(R.id.btnToGoLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToLogin();
+            }
+        });
+
+    }
+
+    public void goToLogin (){
+        Intent intent = new Intent(SelectOptionAuthActivity.this, loguinActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/uberclone/loguinActivity.java
+++ app/src/main/java/com/example/uberclone/loguinActivity.java
@@ -1,14 +1,69 @@
 package com.example.uberclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class loguinActivity extends AppCompatActivity {
+
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mButtonLogin;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loguin);
+
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mButtonLogin = findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
-}
+
+
+    private void login () {
+         String email = mTextInputEmail.getText().toString();
+         String password = mTextInputPassword.getText().toString();
+
+         if (!email.isEmpty() && !password.isEmpty()) {
+             if (password.length() >= 6){
+                 mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                     @Override
+                     public void onComplete(@NonNull Task<AuthResult> task) {
+                         if (task.isSuccessful()) {
+                             Toast.makeText(loguinActivity.this,"El login se realizo exitosamente",Toast.LENGTH_SHORT).show();
+                         }
+                         else {
+                             Toast.makeText(loguinActivity.this,"La email o la contraseña son incorrectos", Toast.LENGTH_SHORT).show();
+                         }
+
+                     }
+                 });
+
+             }
+         }
+
+    }
+    }
