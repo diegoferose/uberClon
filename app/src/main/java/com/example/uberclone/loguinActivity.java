ee96edf5078488535c401ca0253c2789f73fae76
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-02-11 16:08:19 -0500

    15. Progess Dialog

--- app/src/main/java/com/example/uberclone/loguinActivity.java
+++ app/src/main/java/com/example/uberclone/loguinActivity.java
@@ -3,6 +3,7 @@ package com.example.uberclone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import dmax.dialog.SpotsDialog;
+
 public class loguinActivity extends AppCompatActivity {
 
     TextInputEditText mTextInputEmail;
@@ -23,6 +26,7 @@ public class loguinActivity extends AppCompatActivity {
     Button mButtonLogin;
     FirebaseAuth mAuth;
     DatabaseReference mDatabase;
+    AlertDialog mDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +38,8 @@ public class loguinActivity extends AppCompatActivity {
         mButtonLogin = findViewById(R.id.btnLogin);
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        // Instanciamos el progess dialog y colocamos el texto que sale E.C
+        mDialog = new SpotsDialog.Builder().setContext(loguinActivity.this).setMessage("Espere un momento").build();
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,6 +55,7 @@ public class loguinActivity extends AppCompatActivity {
 
          if (!email.isEmpty() && !password.isEmpty()) {
              if (password.length() >= 6){
+                 mDialog.show(); // Muestra el Progess Dialog
                  mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                      @Override
                      public void onComplete(@NonNull Task<AuthResult> task) {
@@ -58,6 +65,7 @@ public class loguinActivity extends AppCompatActivity {
                          else {
                              Toast.makeText(loguinActivity.this,"La email o la contraseña son incorrectos", Toast.LENGTH_SHORT).show();
                          }
+                         mDialog.dismiss(); // No se muestra el Progess Dialog
 
                      }
                  });
