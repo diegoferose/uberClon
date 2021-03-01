a7eee4c902628d19618915ecd451895fc989e035
diegoferose <diegoferose8@gmail.com>
2021-03-01 18:34:11 -0500

    25. Utilizar el GPS para obtener ubicación actual

--- app/src/main/java/com/example/uberclone/activities/client/MapClientActivity.java
+++ app/src/main/java/com/example/uberclone/activities/client/MapClientActivity.java
@@ -1,16 +1,60 @@
 package com.example.uberclone.activities.client;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.uberclone.R;
+import com.example.uberclone.activities.MainActivity;
+import com.example.uberclone.includes.MyToolbar;
+import com.example.uberclone.providers.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
+    private AuthProvider mAuthProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
+        MyToolbar.show(this, "Cliente", false);
+        mAuthProvider = new AuthProvider();
+        mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mMapFragment.getMapAsync(this);
     }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    void logout() {
+        mAuthProvider.logout();
+        Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
--- app/src/main/java/com/example/uberclone/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/example/uberclone/activities/driver/MapDriverActivity.java
@@ -1,10 +1,17 @@
 package com.example.uberclone.activities.driver;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.example.uberclone.R;
+import com.example.uberclone.activities.MainActivity;
+import com.example.uberclone.includes.MyToolbar;
+import com.example.uberclone.providers.AuthProvider;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -12,11 +19,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+    private AuthProvider mAuthProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
+        MyToolbar.show(this, "Conductor", false);
+        mAuthProvider = new AuthProvider();
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
     }
@@ -26,5 +36,26 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            logout();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    void logout() {
+        mAuthProvider.logout();
+        Intent intent = new Intent(MapDriverActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
