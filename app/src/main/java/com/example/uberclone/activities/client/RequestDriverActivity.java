766232b0d2cb1c29179daf1a76da588bc2a3a1b1
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-03-06 18:16:27 -0500

    Seccion 7 capitulo 42

---
+++ app/src/main/java/com/example/uberclone/activities/client/RequestDriverActivity.java
@@ -0,0 +1,30 @@
+package com.example.uberclone.activities.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.uberclone.R;
+
+public class RequestDriverActivity extends AppCompatActivity {
+
+    private LottieAnimationView mAnimation;
+    private TextView mTextViewLookingFor;
+    private Button mButtonCancelRequest;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_driver);
+
+        mAnimation = findViewById(R.id.animation);
+        mTextViewLookingFor = findViewById(R.id.textViewLookingFor);
+        mButtonCancelRequest = findViewById(R.id.btnCancelRequest);
+
+        mAnimation.playAnimation();
+
+    }
+}
