f5792b7a2d42998a067534753f6f503590b11924
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-03-09 00:17:28 -0500

    Seccion 7 capitulo 46

--- app/src/main/java/com/example/uberclone/services/MyFirebaseMessagingClient.java
+++ app/src/main/java/com/example/uberclone/services/MyFirebaseMessagingClient.java
@@ -1,9 +1,22 @@
 package com.example.uberclone.services;
 
-import androidx.annotation.NonNull;
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.uberclone.channel.NotificacionHelper;
+import com.google.android.gms.common.api.Api;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Map;
 
 /*--------------------------------------------------------------------------------------------*/
 /*CLASE: MyFirebaseMessagingClient------------------------------------------------------------*/
@@ -21,5 +34,35 @@ public class MyFirebaseMessagingClient extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        Map<String, String> data = remoteMessage.getData();
+        String title = data.get("title");
+        String body = data.get("body");
+
+
+        if(title != null){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                showNotificationApiOreo(title,body);
+            }
+            else {
+                showNotification(title,body);
+            }
+        }
+    }
+
+    private void showNotification(String title, String body) {
+        PendingIntent intent = PendingIntent.getActivity(getBaseContext(),0, new Intent(), PendingIntent.FLAG_ONE_SHOT);
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificacionHelper notificacionHelper = new NotificacionHelper(getBaseContext());
+        NotificationCompat.Builder builder = notificacionHelper.getNotificationOldAPI(title,body, intent, sound);
+        notificacionHelper.getManager().notify(1, builder.build());
+    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void showNotificationApiOreo(String title, String body) {
+        PendingIntent intent = PendingIntent.getActivity(getBaseContext(),0, new Intent(), PendingIntent.FLAG_ONE_SHOT);
+        Uri sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificacionHelper notificacionHelper = new NotificacionHelper(getBaseContext());
+        Notification.Builder builder = notificacionHelper.getNotification(title, body, intent, sound);
+        notificacionHelper.getManager().notify(1, builder.build());
     }
 }
