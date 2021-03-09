ef0bd9dda267df4660c11be30e2b2b87f6686614
Esteban Cuadros <esteban-cuadros@hotmail.com>
2021-03-08 21:53:29 -0500

    Seccion 7 capitulo 44

---
+++ app/src/main/java/com/example/uberclone/services/MyFirebaseMessagingClient.java
@@ -0,0 +1,20 @@
+package com.example.uberclone.services;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/*--------------------------------------------------------------------------------------------*/
+/*CLASE: MyFirebaseMessagingClient------------------------------------------------------------*/
+/*FUNCION: Contiene los metodos necesarios para realizar los mensajes push--------------------*/
+/*--------------------------------------------------------------------------------------------*/
+
+
+public class MyFirebaseMessagingClient extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+}
